b476e7c1715b314477e6f9e7caf0d2fc667a64b5
Paul <you@msurajpaul13@gmail.com>
2022-09-14 07:21:40 +0000

    Todo Application

--- date.js
+++
@@ -1,9 +0,0 @@
-const { format } = require("date-fns");
-const d = "2021-09-08";
-const xx = new Date("1-2-98");
-try {
-  const formatDate = format(xx, "yyyy-MM-dd");
-  console.log(formatDate);
-} catch (e) {
-  console.log("Invalid Date");
-}
